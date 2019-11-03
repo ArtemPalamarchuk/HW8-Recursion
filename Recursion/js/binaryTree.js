fa325452229cf16d9348318602427f7fd3268cfa
Foks991 <33574193+Foks991@users.noreply.github.com>
2019-11-03 20:13:34 +0200

    Update binaryTree.js

--- Recursion/js/binaryTree.js
+++ Recursion/js/binaryTree.js
@@ -15,15 +15,79 @@ class BinaryTreeNode {
 const binaryTree = new BinaryTree();
 const binaryTreeNode = new BinaryTreeNode();
 
-BinaryTree.prototype.push = function (element) {
-	const noda = new BinaryTreeNode(element);
-	if (this.root == null){
-		this.root = noda;
-		return;
-	}
-	const tmpNoda = this.root;
-	if(tmpNoda.left == null){
-		tmpNoda.left = noda;
-	}
+BinnaryTree.prototype.search = function (searchEl , nextStep) {
+
+    let tmpNode;
+    if(nextStep == undefined){
+        tmpNode = this.root;
+    }
+    else{
+        tmpNode = nextStep;
+    }
+
+    if(tmpNode.data == searchEl){
+        tmpNode.data;
+    }
+    else if( searchEl < tmpNode.data && tmpNode.left != null ){
+        return this.search(searchEl, tmpNode.left)
+    }
+    else if(searchEl > tmpNode.data && tmpNode.right != null ){
+        return this.search(searchEl, tmpNode.right)
+    }
+    else{
+
+        return searchEl;
+    }
 };
 
+BinnaryTree.prototype.push = function (el, nextStep)  {
+    const noda = new CreateTreeNode(el);
+    let tmpNoda;
+    if(nextStep === undefined){
+        tmpNoda  = this.root;
+    }
+    else {
+        tmpNoda = nextStep
+    }
+
+    if(this.root == null){
+        this.root = noda;
+    }
+    else {
+        if (el >= tmpNoda.data && tmpNoda.right != null) {
+            return this.push(el, tmpNoda.right)
+        } else if (el < tmpNoda.data && tmpNoda.left != null) {
+            return this.push(el, tmpNoda.left)
+        }else if(el >= tmpNoda.data){
+            tmpNoda.right = noda;
+        }else if(el < tmpNoda.data){
+            tmpNoda.left = noda;
+        }
+    }
+};
+
+BinnaryTree.prototype.push = function (el, nextStep)  {
+    const noda = new CreateTreeNode(el);
+    let tmpNoda;
+    if(nextStep === undefined){
+        tmpNoda  = this.root;
+    }
+    else {
+        tmpNoda = nextStep
+    }
+
+    if(this.root == null){
+        this.root = noda;
+    }
+    else {
+        if (el >= tmpNoda.data && tmpNoda.right != null) {
+            return this.push(el, tmpNoda.right)
+        } else if (el < tmpNoda.data && tmpNoda.left != null) {
+            return this.push(el, tmpNoda.left)
+        }else if(el >= tmpNoda.data){
+            tmpNoda.right = noda;
+        }else if(el < tmpNoda.data){
+            tmpNoda.left = noda;
+        }
+    }
+};
